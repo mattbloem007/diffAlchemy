9b76569a0dfa40095f1cd152666d9e99986bc40b
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2019-12-09 18:03:08 +0200

    latest working noq

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import BlogItems from "../components/items-blog";
 
 class BlogList extends React.Component {

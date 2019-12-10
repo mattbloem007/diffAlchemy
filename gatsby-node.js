0a2e19b561513d1185c20008b02f5134483ed33a
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2019-12-10 20:22:51 +0200

    npm i

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,5 @@
 const { createFilePath, createRemoteFileNode } = require(`gatsby-source-filesystem`);
 const path = require(`path`);
-
 exports.createPages = ({ graphql, actions }) => {
     const { createPage } = actions;
 
